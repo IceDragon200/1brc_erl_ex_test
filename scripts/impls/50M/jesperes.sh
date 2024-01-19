353f0db8f6c12284028f1743b1de886d1452b0af
Corey Powell <mistdragon100@gmail.com>
2024-01-19 10:17:20 -0500

    Updated IceDragon200 implementation and add implementation scripts for jesperes using garazdawi's repo

---
+++ scripts/impls/50M/jesperes.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S bash -x
+source ./common.sh
+mkdir -p "${outdir}"
+data_path=$1
+
+# This is not an error, garazdawi's implementation was based off jesperes'
+# he left the original aggregate.erl intact so we can run it via his scaffold
+cd "${impldir}/garazdawi/erlang_1brc"
+ln -sf "${data_path}" ./measurements.txt
+($branchcmd && MODULE=aggregate $timecmd ./run.sh ./measurements.txt 2>&1) > "${outdir}/jesperes.50M.txt"
+cd "${owd}"
