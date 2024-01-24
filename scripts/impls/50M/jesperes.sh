fe43c81aa7e70732b478d73f51c0c0e0af11a2d3
Jesper Eskilson <jesper.eskilson@klarna.com>
2024-01-24 11:14:23 +0100

    Update jesperes solution

--- scripts/impls/50M/jesperes.sh
+++ scripts/impls/50M/jesperes.sh
@@ -3,9 +3,7 @@ source ./common.sh
 mkdir -p "${outdir}"
 data_path=$1
 
-# This is not an error, garazdawi's implementation was based off jesperes'
-# he left the original aggregate.erl intact so we can run it via his scaffold
-cd "${impldir}/garazdawi/erlang_1brc"
+cd "${impldir}/jesperes/erlang_1brc"
 ln -sf "${data_path}" ./measurements.txt
-($branchcmd && $timecmd ./run.sh ./measurements.txt "aggregate" 2>&1) > "${outdir}/jesperes.50M.txt"
+($branchcmd && $timecmd ./run.sh ./measurements.txt 2>&1) > "${outdir}/jesperes.50M.txt"
 cd "${owd}"
