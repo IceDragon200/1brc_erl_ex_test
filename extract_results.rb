95260f307053a7b84acd9766bb8828210aad7655
Corey Powell <mistdragon100@gmail.com>
2024-01-29 22:17:21 -0500

    If a result doesn't have a time assume it's 99:99:99
    Which means it was super slow

--- extract_results.rb
+++ extract_results.rb
@@ -3,6 +3,14 @@ out_dir = ARGV.first
 results_1b = {}
 results_50m = {}
 
+def sort_results(results)
+  keys = results.keys
+  keys.sort_by do |key|
+    row = results[key]
+    (row && row[0]) || "99:99:99"
+  end
+end
+
 Dir.glob(File.join(out_dir, "*.txt")).each do |filename|
   time = nil
   cpu = nil
@@ -12,13 +20,14 @@ Dir.glob(File.join(out_dir, "*.txt")).each do |filename|
   File.read(filename).each_line do |line|
     case line
     when /\A\s+Elapsed \(wall clock\) time \(h:mm:ss or m:ss\):\s+(.+)\n/
-      time = $1
+      time = $1.rjust(8, '0')
 
     when /\A\s+Percent of CPU this job got:\s+(.+)\n/
       cpu = $1
 
     when /\A\s+Maximum resident set size \(kbytes\):\s+(.+)\n/
       mem = $1
+
     when /\A\s+Exit status:\s+(\d+)\n/
       exit_status = ($1).to_i
 
@@ -50,7 +59,7 @@ rows = [
 template = "| %40s | %10s | %10s | %12s | %9s |"
 
 # Sort keys based on the Time (50M) in ascending order
-sorted_keys = keys.sort_by { |key| results_50m[key][0] }
+sorted_keys = sort_results(results_50m)
 
 sorted_keys.each do |key|
   time50M, cpu50M, mem50M, exitStatus50M = *results_50m[key]
@@ -79,7 +88,7 @@ rows = [
 template = "| %40s | %10s | %10s | %12s | %9s |"
 
 # Sort keys based on the Time (1B) in ascending order
-sorted_keys = keys.sort_by { |key| results_1b[key][0] }
+sorted_keys = sort_results(results_1b)
 
 sorted_keys.each do |key|
   time1B, cpu1B, mem1B, exitStatus1B = *results_1b[key]
